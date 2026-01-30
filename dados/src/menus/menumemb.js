652d1641a02f89ca91425e64868d4926d17a789b
Bea Souza <158209681+souzabweatriz@users.noreply.github.com>
2026-01-30 19:38:00 -0300

    first commit

--- dados/src/menus/menumemb.js
+++ dados/src/menus/menumemb.js
@@ -28,7 +28,6 @@ ${middleBorder}${menuItemIcon}${prefix}statusbot
 ${middleBorder}${menuItemIcon}${prefix}statusgp
 ${middleBorder}${menuItemIcon}${prefix}regras
 ${middleBorder}${menuItemIcon}${prefix}zipbot
-${middleBorder}${menuItemIcon}${prefix}gitbot
 ${bottomBorder}
 
 ${menuTopBorder}${separatorIcon} *${personalMenuTitle}*
